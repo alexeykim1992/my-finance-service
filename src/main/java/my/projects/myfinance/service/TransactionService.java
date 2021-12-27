a8721f68ebcd75f4999e81cbb66e867a79fc7436
Aleksey Kim <alexmail1992@gmail.com>
2021-12-27 15:27:49 +0500

    Add currency support on add/edit transaction

--- src/main/java/my/projects/myfinance/service/TransactionService.java
+++ src/main/java/my/projects/myfinance/service/TransactionService.java
@@ -2,7 +2,9 @@ package my.projects.myfinance.service;
 
 import my.projects.myfinance.dto.TransactionDto;
 import my.projects.myfinance.dto.TransactionRequestDto;
+import my.projects.myfinance.model.Account;
 import my.projects.myfinance.model.Transaction;
+import my.projects.myfinance.repo.AccountRepo;
 import my.projects.myfinance.repo.TransactionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,8 @@ public class TransactionService {
     TransactionRepo transactionRepo;
     @Autowired
     UserService userService;
+    @Autowired
+    AccountRepo accountRepo;
 
     public Transaction getTransaction(Long transactionId) {
         return transactionRepo.findFirstByIdAndUserId(
@@ -64,15 +68,21 @@ public class TransactionService {
     }
 
     public Long addTransaction(TransactionRequestDto request) {
+        Account fromAccount = accountRepo.findFirstByIdAndUserId(
+                request.getFrom().longValue(), userService.getCurrentUserId());
+        Account toAccount = accountRepo.findFirstByIdAndUserId(
+                request.getTo().longValue(), userService.getCurrentUserId());
+        if (fromAccount == null || toAccount == null) return -1L;
+
         Transaction transaction = new Transaction()
                 .setDate(request.getDate())
                 .setUserId(userService.getCurrentUserId())
                 .setSourceId(request.getFrom())
                 .setSourceValue(request.getFromValue())
-                .setSourceCurrency(1)
+                .setSourceCurrency(fromAccount.getCurrency())
                 .setDestinationId(request.getTo())
                 .setDestinationValue(request.getToValue())
-                .setDestinationCurrency(1)
+                .setDestinationCurrency(toAccount.getCurrency())
                 .setDescription(request.getDescription())
                 .setCreationDate(new Timestamp(System.currentTimeMillis()));
         return transactionRepo.save(transaction).getId();
@@ -80,16 +90,24 @@ public class TransactionService {
 
     public Long editTransaction(TransactionRequestDto request) {
         Transaction transaction = this.getTransaction(request.getId());
-        if (transaction != null) {
-            transaction.setDate(request.getDate())
-                    .setSourceId(request.getFrom())
-                    .setDestinationId(request.getTo())
-                    .setSourceValue(request.getFromValue())
-                    .setDestinationValue(request.getToValue())
-                    .setDescription(request.getDescription());
-            return transactionRepo.save(transaction).getId();
-        }
-        return -1L;
+        Account fromAccount = accountRepo.findFirstByIdAndUserId(
+                request.getFrom().longValue(), userService.getCurrentUserId());
+        Account toAccount = accountRepo.findFirstByIdAndUserId(
+                request.getTo().longValue(), userService.getCurrentUserId());
+        if (transaction == null || fromAccount == null || toAccount == null)
+            return -1L;
+
+        transaction
+                .setDate(request.getDate())
+                .setSourceId(request.getFrom())
+                .setSourceValue(request.getFromValue())
+                .setSourceCurrency(fromAccount.getCurrency())
+                .setDestinationId(request.getTo())
+                .setDestinationValue(request.getToValue())
+                .setDestinationCurrency(toAccount.getCurrency())
+                .setDescription(request.getDescription());
+        return transactionRepo.save(transaction).getId();
+
     }
 
     public Long deleteTransaction(TransactionRequestDto request) {
